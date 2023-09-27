cd5f2ddd1cc72887a1e0a2238d4b018ef1a8557d
Himalay Chauhan <himalay.chauhan@tibilsolutions.com>
2023-09-27 17:19:23 +0530

    Added NavBar Component and assets images

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { Header, List } from "semantic-ui-react";
+import { Container, List } from "semantic-ui-react";
 import { Activity } from "../models/activity";
+import NavBar from "./NavBar";
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
@@ -14,14 +15,16 @@ function App() {
       });
   }, []);
   return (
-    <div>
-      <Header as={"h2"} icon={"users"} content="Reactivities" />
-      <ul>
-        {activities.map((activity) => (
-          <List.Item key={activity.id}>{activity.title}</List.Item>
-        ))}
-      </ul>
-    </div>
+    <>
+      <NavBar></NavBar>
+      <Container style={{ marginTop: "7em" }}>
+        <List>
+          {activities.map((activity) => (
+            <List.Item key={activity.id}>{activity.title}</List.Item>
+          ))}
+        </List>
+      </Container>
+    </>
   );
 }
 
