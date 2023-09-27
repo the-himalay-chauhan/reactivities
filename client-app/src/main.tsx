2fdec01bfc364223329ee8ddab665f58181987b5
Himalay Chauhan <himalay.chauhan@tibilsolutions.com>
2023-09-27 16:45:18 +0530

    Organized files and folders and added interface for activity

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import 'semantic-ui-css/semantic.min.css'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "semantic-ui-css/semantic.min.css";
+import "./app/layout/styles.css";
+import App from "./app/layout/App";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
